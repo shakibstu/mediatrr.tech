6e736ead3e56a92fe88f62b323b7ccaeab743964
DESKTOP-17MS7J6\Shakib <shakibstu@hotmail.com>
2025-12-07 13:55:12 +0100

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Docs from './pages/Docs';
+import Footer from './components/Footer';
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/docs/*" element={<Docs />} />
-      </Routes>
+      <div className="app-layout">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/docs/*" element={<Docs />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
